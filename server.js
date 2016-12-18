880ef7ba004ac8631c2665291db2b3a0cf96e838
Larry Luangrath <larry.luangrath@clynch.com>
2016-12-18 09:46:50 -0600

    add fs code

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 var httpServer = express();
 var httpPort = process.env.PORT || 8080;
 
 httpServer.use(bodyParser.json());
 httpServer.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res){
+    console.log('GET /')
+    var html = fs.readFileSync('index.html');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
+});
 
 httpServer.post('/service', function(req, res){
     console.dir(req.body);
