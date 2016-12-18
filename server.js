2811721567bddd57fe5dc5aeb6e4fc5abda3a285
Larry Luangrath <larry.luangrath@clynch.com>
2016-12-18 15:57:33 -0600

    adding email stuff

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const WebSocket = require('ws');
+const postmark = require("postmark")(process.env.POSTMARK_API_TOKEN);
 
 var httpServer = express();
 var httpPort = process.env.PORT || 8080;
@@ -31,6 +32,24 @@ httpServer.post('/service', function(req, res){
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end('<Response><Message>SMS received by Server</Message></Response>');
 });
+
+httpServer.post('sendOwnerEmail',function(req, res){
+  postmark.send({
+    "From": "leonard@bigbangtheory.com",
+    "To": "sheldon@bigbangtheory.com",
+    "Subject": "Hello from Postmark",
+    "TextBody": "Hello!",
+    "Tag": "big-bang"
+  }, function(error, success) {
+      if(error) {
+          console.error("Unable to send via postmark: " + error.message);
+        return;
+      }
+      console.info("Sent to postmark for delivery")
+  });
+
+});
+
 
 httpServer.listen(httpPort, function () {
   console.log("App now running on port", httpPort);
